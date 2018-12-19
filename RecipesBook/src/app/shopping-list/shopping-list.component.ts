daefa027274900267b94ba498b66c26c514e052d
l-bevc <lbevc@pmfst.hr>
2018-12-19 15:10:48 +0100

    page layout using bootstrap

--- RecipesBook/src/app/shopping-list/shopping-list.component.ts
+++ RecipesBook/src/app/shopping-list/shopping-list.component.ts
@@ -6,7 +6,10 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
   constructor() { }
 
   ngOnInit() {
